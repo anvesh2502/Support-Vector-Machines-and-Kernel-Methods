c4566b6bcaadb585d0acbf5dfa4ac8633b2c595a
anvesh2502 <anveshrocks@yahoo.co.in>
2016-07-28 17:59:26 -0500

    Added linear classifier

--- advancedclassify.py
+++ advancedclassify.py
@@ -17,6 +17,34 @@ def loadmatch(f,allnum=False) :
             rows.append(matchrow(line.split(','),allnum))
         return rows
 
+def lineartrain(rows) :
+
+    averages={}
+    counts={}
+
+    for row in rows :
+        # Get the class of this point
+        cl=row.match
+
+        averages.setdefault(cl,[0.0]*(len(row.data)))
+        counts.setdefault(cl,0)
+
+        # Add this point to the averages
+        for i in range(len(row.data)) :
+            averages[cl][i]+=float(row.data[i])
+
+        counts[cl]+=1
+
+    # Divide sums by counts to get the averages
+    for cl,avg in averages.items() :
+        for i in range(len(avg)) :
+            avg[i]/=counts[cl]
+
+    return averages                
+
+
+
+
 def plotagematches(rows) :
 
     xdm,ydm=[r.data[0] for r in rows if r.match==1],[r.data[1] for r in rows if r.match==1]
